e100678edccfdea2e4d7728d8a19548e1147056d
Mahmoud Nasser <mnassersalah@gmail.com>
2022-11-03 22:08:03 +0200

    Skinet - add solution & API project

---
+++ Skinet/Skinet.API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skinet.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProductsController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("/{id}")]
+        public string Get(int id)
+        {
+            return $"product with id : {id}";
+        }
+
+
+        [HttpGet]
+        public string Get()
+        {
+            return "all products";
+        }
+
+
+    }
+}
