2cfec74447f18cda1fa21bf87f9b1d31077cb5e7
Mahmoud Nasser <mnassersalah@gmail.com>
2022-11-03 23:46:58 +0200

    Skinet - API - add initial dbcontext & entity

--- Skinet/Skinet.API/Program.cs
+++ Skinet/Skinet.API/Program.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Skinet.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var config = builder.Configuration;
+builder.Services.AddDbContext<StoreContext>(optionsBuilder => 
+    optionsBuilder.UseSqlite(config.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
